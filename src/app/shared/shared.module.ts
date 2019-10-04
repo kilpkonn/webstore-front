69fb58e31d9804a50ab23d6f52d5e40dbf13ea12
taannu <tavo.annus@gmail.com>
2019-10-04 21:31:04 +0300

    Add contact page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
-//import { AppMaterialModule} from "../modules/app-material/app-material.module";
-
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
-    //AppMaterialModule,
   ]
 })
 export class SharedModule { }
