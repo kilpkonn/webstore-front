201d02742bcebf45841e55e00a2d0871a129fd59
taannu <tavo.annus@gmail.com>
2019-10-10 21:07:57 +0300

    Better look for news

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home.component';
+import { AppMaterialModule } from "../app-material/app-material.module";
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    AppMaterialModule
   ]
 })
 export class HomeModule { }
