b99ca8fd59c359647ec9226c28186e50c241edc8
taannu <tavo.annus@gmail.com>
2019-10-12 12:31:00 +0300

    Show proper parsed date

--- src/app/modules/home/components/home.component.ts
+++ src/app/modules/home/components/home.component.ts
@@ -12,10 +12,6 @@ export class HomeComponent implements OnInit {
 
   constructor(private newsService: NewsService) { }
 
-  parseDate(date: string) : Date{
-    return new Date(date);
-  }
-
   getNews(): void {
     this.newsService.getNews()
       .subscribe(news => this.news = news);
