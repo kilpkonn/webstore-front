ae1e6ef85636da841753621dcf4996d4c0262cfb
taannu <tavo.annus@gmail.com>
2019-10-13 13:30:01 +0300

    Fix product spacing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,20 @@ import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { AppMaterialModule } from "../modules/app-material/app-material.module";
 import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "../app-routing.module";
 
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     AppMaterialModule,
     RouterModule,
+    AppRoutingModule
   ]
 })
 export class SharedModule { }
