454f5b94e45d3cccfb18547b4c7af4fa56703bba
taannu <tavo.annus@gmail.com>
2019-10-16 21:56:08 +0300

    Add possibility to delete news

--- src/app/modules/home/components/home.component.ts
+++ src/app/modules/home/components/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 import { NewsService } from '../services/news.service';
 import { News } from '../../../shared/models/news';
 import { slider } from '../../../shared/animations/route-animations';
@@ -18,15 +20,30 @@ export class HomeComponent implements OnInit {
   show = false;
   news: News[];
 
-  constructor(private newsService: NewsService) { }
+  constructor(private newsService: NewsService,
+              private matIconRegistry: MatIconRegistry,
+              private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'add_circle',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../assets/add_circle.svg')
+    );
+  }
+
+  ngOnInit() {
+    this.getNews();
+  }
 
   getNews(): void {
     this.newsService.getNews()
       .subscribe(news => this.news = news);
   }
 
-  ngOnInit() {
-    this.getNews();
+  removeNews(news: News) {
+    this.newsService.deleteNews(news.id)
+      .subscribe(data => {
+        this.news.splice(this.news.indexOf(news), 1);
+        // Verify by pulling new products?
+      }, error => console.log(error));
   }
 
   onAnimationFinished() {
