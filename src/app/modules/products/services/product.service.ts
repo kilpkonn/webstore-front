587b924b0d22bd70a8396ee322d70d8bfcd7d634
tavo <taannu@ttu.ee>
2019-10-16 12:28:08 +0300

    more renaming

--- src/app/modules/products/services/product.service.ts
+++ src/app/modules/products/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../../../shared/models/product';
-import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
