f3e834e51c8d0aa3eebb1c1baa56aef4b1904ba9
tajoot <tajoot@ttu.ee>
2019-10-16 18:19:59 +0300

    Add form based toolbar

--- src/app/modules/products/services/product.service.ts
+++ src/app/modules/products/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../../../shared/models/product';
+import {Params} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -19,9 +20,22 @@ export class ProductService {
     return this.http.get<Product>(`${this.url}/${id}`);
   }
 
-  getFilteredProducts(category: string): Observable<Product[]> {
-    return this.http.get<Product[]>(this.url, {
-      params: new HttpParams().set('category', category)});
+  getFilteredProducts(params: Params) {
+    console.log(params.category);
+    let prms = new HttpParams();
+    if (params.category !== undefined) {
+      prms = prms.set('category', params.category);
+    }
+    if (params.name !== undefined) {
+      prms = prms.set('name', params.name);
+    }
+    if (params.sort !== undefined) {
+      prms = prms.set('sort', params.sort);
+    }
+    if (params.order !== undefined) {
+      prms = prms.set('order', params.order);
+    }
+    return this.http.get<Product[]>(this.url, {params: prms});
   }
 
   getImage(id: number): Observable<Blob> {
