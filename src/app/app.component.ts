9594b2150e67125ab13ffde487141dca45482b8f
taannu <tavo.annus@gmail.com>
2019-10-17 09:29:24 +0300

    cleanup for animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { fader, slider } from './shared/animations/route-animations';
+import { slider } from './shared/animations/route-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    slider,
-    // fader
+    slider
   ]
 })
 export class AppComponent {
