91ecf1ea965f401a1716a4e57e9340ad60822c07
taannu <tavo.annus@gmail.com>
2019-11-12 17:17:41 +0200

    Test nginx docker container

---
+++ scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Pulling image"
+docker pull "$CI_REGISTRY_USER"/"$CI_REGISTRY_REPOSITORY":"$CI_COMMIT_SHORT_SHA"
+
+echo "Moving container $APP_CONTAINER_NAME to $APP_CONTAINER_NAME-old"
+docker rename "$APP_CONTAINER_NAME" "$APP_CONTAINER_NAME-old"
+
+# TODO: use different ports etc for rolling upgrade
+echo "Stopping container $APP_CONTAINER_NAME-old"
+docker container ls -a -s
+docker stop "$APP_CONTAINER_NAME-old" || true
+echo "Removing $APP_CONTAINER_NAME-old"
+docker rm "$APP_CONTAINER_NAME-old" || true
+docker container ls -a -s
+
+echo "Creating network bridge for proxy (if none exsists)"
+docker network create --driver bridge proxy-network || true # Create only if none exists
+
+echo "Starting new container: $APP_CONTAINER_NAME"
+docker run \
+   --name "$APP_CONTAINER_NAME" \
+   -p 80:80 \
+   --network="proxy-network" \
+   --restart=always \
+   -d "$CI_REGISTRY_USER"/"$CI_REGISTRY_REPOSITORY":"$CI_COMMIT_SHORT_SHA"
+
+docker container ls -a -s
+
+echo "Removing old images"
+docker image ls
+# shellcheck disable=SC2046
+docker rmi $(docker images | grep "$CI_REGISTRY_USER"/"$CI_REGISTRY_REPOSITORY" | awk '{print $3}')
+docker image ls
