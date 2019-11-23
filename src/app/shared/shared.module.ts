de58883d2363913119444fcadc274abd062c31f7
tajoot <tajoot@ttu.ee>
2019-11-23 09:31:24 +0200

    Add sidenav to menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,21 +5,24 @@ import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { AppMaterialModule } from '../modules/app-material/app-material.module';
 import { DeleteButtonComponent } from './components/delete-button/delete-button.component';
-import {FlexModule} from "@angular/flex-layout";
+import {ExtendedModule, FlexModule} from '@angular/flex-layout';
+import { SidenavListComponent } from './layout/sidenav-list/sidenav-list.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, DeleteButtonComponent],
+  declarations: [HeaderComponent, FooterComponent, DeleteButtonComponent, SidenavListComponent],
   exports: [
     HeaderComponent,
     FooterComponent,
-    DeleteButtonComponent
+    DeleteButtonComponent,
+    SidenavListComponent
   ],
   imports: [
     CommonModule,
     AppMaterialModule,
     RouterModule,
     FlexModule,
+    ExtendedModule,
   ]
 })
 export class SharedModule { }
