03dabbdb541b1b2d6cdec3fdfb534ce95d81b2a6
tajoot <tajoot@ttu.ee>
2019-12-04 14:12:52 +0200

    Add login / logout to sidenav

--- src/app/shared/layout/sidenav-list/sidenav-list.component.ts
+++ src/app/shared/layout/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {User} from '../../models/user';
+import {AuthenticationService} from '../../services/authentication.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -6,14 +9,26 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./sidenav-list.component.css']
 })
 export class SidenavListComponent implements OnInit {
+  user: User;
+
   @Output() sidenavClose = new EventEmitter();
 
-  constructor() { }
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService) {
+    this.user = authenticationService.currentUserValue;
+    authenticationService.getUser.subscribe(user => this.user = user);
+  }
 
   ngOnInit() {
   }
 
   public onSidenavClose = () => {
     this.sidenavClose.emit();
+  }
+
+  logout() {
+    this.onSidenavClose();
+    this.authenticationService.logout();
+    this.router.navigate(['/user/login']);
   }
 }
