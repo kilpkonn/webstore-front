f67bada09bdac2968747c006067214629788d7ee
tajoot <tajoot@ttu.ee>
2019-12-05 10:52:34 +0200

    Improve login/register form design

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -6,17 +6,19 @@ import { LoginComponent } from './components/login/login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppMaterialModule } from '../app-material/app-material.module';
 import {UserRoutingModule} from './user-routing.module';
+import {FlexModule} from '@angular/flex-layout';
 
 
 
 @NgModule({
   declarations: [RegisterComponent, UserComponent, LoginComponent],
-  imports: [
-    CommonModule,
-    UserRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    AppMaterialModule
-  ]
+    imports: [
+        CommonModule,
+        UserRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        AppMaterialModule,
+        FlexModule
+    ]
 })
 export class UserModule { }
