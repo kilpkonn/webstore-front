950fa10c79e66cf1b8a49d06dc5c0075b00fd680
taannu <tavo.annus@gmail.com>
2019-12-06 15:00:27 +0200

    still broken

--- src/app/modules/products/services/image.service.ts
+++ src/app/modules/products/services/image.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { FileInput } from 'ngx-material-file-input';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +9,17 @@ export class ImageService {
 
   private uploadUrl = 'api/upload/image';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   uploadImage(image: File): Observable<Object> {
     console.log(image);
     const formData: FormData = new FormData();
-    formData.append('file', image, image.name);
-    return this.http.post(this.uploadUrl, image);
+    formData.append('file', image);
+    return this.http.post(this.uploadUrl, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data;boundary=' + image.size,
+      }
+    });
   }
 }
