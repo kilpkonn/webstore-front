e5b1d213ac9ee4ac1a36f1f39e450568a0c820e5
taannu <taannu@ttu.ee>
2019-12-12 18:44:40 +0200

    Fix image padding being overridden

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,30 +12,41 @@ export Red='\033[0;31m'          # Red
 export Green='\033[0;32m'        # Green
 export Yellow='\033[0;33m'       # Yellow
 export Blue='\033[0;34m'         # Blue
-export Purple='\033[1;95m'       # Purple
+export Purple='\033[0;35m'       # Purple
 export Cyan='\033[0;36m'         # Cyan
 
-echo -e "${Cyan}Copying placeholder.jpg to ~/images...${Color_Off}"
+# High Intensity
+export IBlack='\033[0;90m'       # Black
+export IRed='\033[0;91m'         # Red
+export IGreen='\033[0;92m'       # Green
+export IYellow='\033[0;93m'      # Yellow
+export IBlue='\033[0;94m'        # Blue
+export IPurple='\033[0;95m'      # Purple
+export ICyan='\033[0;96m'        # Cyan
+export IWhite='\033[0;97m'       # White
+
+echo -e "${ICyan}Copying placeholder.jpg to ~/images...${IRed}"
 sudo cp images/placeholder.jpg ~/images
 
-echo -e "${Cyan}Pulling image ${Color_Off}"
+echo -e "${ICyan}Pulling image ${IBlue}"
 docker pull "$CI_REGISTRY_USER"/"$CI_REGISTRY_REPOSITORY":"$CI_COMMIT_SHORT_SHA"
 
-echo -e "${Cyan}Moving container${Yellow} $APP_CONTAINER_NAME ${Cyan}to ${Yellow}$APP_CONTAINER_NAME-old ${Color_Off}"
+echo -e "${ICyan}Moving container${IYellow} $APP_CONTAINER_NAME ${Cyan}to ${IYellow}$APP_CONTAINER_NAME-old ${Color_Off}"
 docker rename "$APP_CONTAINER_NAME" "$APP_CONTAINER_NAME-old"
 
 # TODO: use different ports etc for rolling upgrade
-echo -e "${Cyan}Stopping container $APP_CONTAINER_NAME-old${Color_Off}"
+echo -e "${ICyan}Stopping container $APP_CONTAINER_NAME-old${Yellow}"
 docker container ls -a -s
 docker stop "$APP_CONTAINER_NAME-old" || true
-echo -e "${Cyan}Removing $APP_CONTAINER_NAME-old${Color_Off}"
+
+echo -e "${ICyan}Removing $APP_CONTAINER_NAME-old${Purple}"
 docker rm "$APP_CONTAINER_NAME-old" || true
 docker container ls -a -s
 
-echo -e "${Cyan}Creating internal network bridge for proxy-back-database (if none exsists)${Color_Off}"
+echo -e "${ICyan}Creating internal network bridge for proxy-back-database (if none exsists)${IRed}"
 docker network create --driver bridge api-internal-network || true # Create only if none exists
 
-echo -e "${Cyan}Starting new container: $APP_CONTAINER_NAME${Color_Off}"
+echo -e "${ICyan}Starting new container: $APP_CONTAINER_NAME${IPurple}"
 docker run \
    --name "$APP_CONTAINER_NAME" \
    -p 80:80 \
@@ -50,9 +61,10 @@ echo -e "${Green}"
 docker container ls -a -s
 echo -e "${Color_Off}"
 
-echo -e "${Cyan}Removing old images${Color_Off}"
+echo -e "${Cyan}Removing old images${Yellow}"
 docker image ls
 echo -e "${Purple}"
 docker system prune -a -f # Needed for unnamed images / containers / etc
-echo -e "${Color_Off}"
+echo -e "${Green}"
 docker image ls
+echo -e "${Color_Off}"
