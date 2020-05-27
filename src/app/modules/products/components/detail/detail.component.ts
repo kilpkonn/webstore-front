cfa7f9f9661b4a33cbffea55d538d406031371f0
taannu <tavo.annus@gmail.com>
2020-05-27 16:00:23 +0300

    Prepare for edit mode

--- src/app/modules/products/components/detail/detail.component.ts
+++ src/app/modules/products/components/detail/detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../../shared/models/product';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '../../../../shared/services/authentication.service';
+import { User } from '../../../../shared/models/user';
 
 @Component({
   selector: 'product-detail',
@@ -10,10 +12,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class DetailComponent implements OnInit {
   product: Product;
+  user: User;
+  isEditingMode = false;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
-              private productService: ProductService) {
+              private productService: ProductService,
+              private authenticationService: AuthenticationService
+  ) {
+    this.user = authenticationService.currentUserValue;
+    authenticationService.getUser.subscribe(user => this.user = user);
   }
 
   getProduct() {
